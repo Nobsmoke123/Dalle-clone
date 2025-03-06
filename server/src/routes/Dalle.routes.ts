6cbf0118904f94b02404c630bf1f2551577dc4e4
Nobsmoke123 <donaldakobundu@gmail.com>
2025-03-06 04:59:35 +0100

    update routes; add hello world responses to Dalle and Post routes, and enhance .gitignore to include log files

--- server/src/routes/Dalle.routes.ts
+++ server/src/routes/Dalle.routes.ts
@@ -1,5 +1,9 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 const router = Router();
+
+router.get("/", (_req: Request, res: Response) => {
+  res.send("Hello World from Dalle Routes.");
+});
 
 export default router;
